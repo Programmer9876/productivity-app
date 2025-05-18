77e17abaee561d9752c655a3cc93a4406f304721
Programmer9876 <xu0chi@gmail.com>
2025-05-18 14:24:38 -0400

    Remove .env from Git tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+// src/App.tsx
+import { useState, useEffect } from "react";
 import { db } from "./firebase";
-import { ref, push } from "firebase/database";
+import { ref, push, onValue } from "firebase/database";
+
+interface Entry {
+  text: string;
+  timestamp: number;
+}
 
 function App() {
   const [entry, setEntry] = useState<string>("");
+  const [entries, setEntries] = useState<Entry[]>([]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
     if (!entry.trim()) return;
 
     const entriesRef = ref(db, "entries");
@@ -18,6 +24,33 @@ function App() {
 
     setEntry("");
   };
+  //react hook
+  useEffect(() => {
+    //create reference to the enteries
+    const entriesRef = ref(db, "entries");
+    //starts listening  navagate away when component gone
+    const unsubscribe = onValue(entriesRef, (snapshot) => {
+      //get actual json object from firebase
+      
+      const data = snapshot.val();
+      const loaded: Entry[] = [];
+
+      if (data) {
+        //loop through starting with newest entry
+        Object.values(data).forEach((item) => {
+          //newest is first in
+          loaded.push(item as Entry);
+        });
+
+        // Sort newest to last and oldest to first
+        loaded.sort((a, b) => b.timestamp - a.timestamp);
+      }
+      //update entries 
+      setEntries(loaded);
+    });
+    //close the listener
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div className="p-4 max-w-md mx-auto">
@@ -28,7 +61,7 @@ function App() {
           className="border p-2 w-full mb-2"
           value={entry}
           onChange={(e) => setEntry(e.target.value)}
-          placeholder="e.g. Studied DSA for 2 hours"
+          placeholder="e.g. Ran 3 miles, read 20 pages"
         />
         <button
           type="submit"
@@ -37,6 +70,21 @@ function App() {
           Log Entry
         </button>
       </form>
+      
+      <h2 className="text-xl font-semibold mt-6 mb-2">Recent Entries:</h2>
+      <ul className="space-y-2">
+        {entries.map((e, i) => (
+          // for each element give key to react to unique track and show actual entry text
+          // as well as show timestamp
+          <li key={i} className="border p-2 rounded bg-gray-100">
+            <div>{e.text}</div>
+            <div className="text-xs text-gray-500">
+              {new Date(e.timestamp).toLocaleString()}
+               
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
